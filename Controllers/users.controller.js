192181823aa17cbac0d23803c31ea536a91acf73
Juan Cruz <j.cruzb@outlook.com>
2023-09-25 21:48:04 +0200

    Login

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -1,5 +1,7 @@
 //controladores de usuarios
 const User = require('../Models/User.model');
+const { StatusCodes } = require('http-status-codes');
+const createHttpError = require('http-errors');
 
 module.exports.create = (req, res, next) => {
     if (req.file) {
@@ -8,12 +10,37 @@ module.exports.create = (req, res, next) => {
     
     const user = new User(req.body);
     user.save()
-        .then((user) => res.status(201).json(user))
+        .then((user) => {
+            if(!user) {
+                next(createHttpError(StatusCodes.BAD_REQUEST, 'User not created'))
+            } else {
+                res.status(StatusCodes.CREATED).json(user)
+            }
+        })
         .catch(next);
 }
 
 module.exports.list = (req, res, next) => {
     User.find()
-        .then((users) => res.status(200).json(users))
+        .then((users) => {
+            if(!users) {
+                next(createHttpError(StatusCodes.NOT_FOUND, 'Users not found'))
+            } else {
+                res.status(StatusCodes.OK).json(users)
+            }
+        })
         .catch(next);
 }
+
+module.exports.getCurrentUser = (req, res, next) => {
+    console.log('controlador');
+    User.findById(req.currentUser)
+      .then(user => {
+        if (!user) {
+          next(createHttpError(StatusCodes.NOT_FOUND, 'User not found'))
+        } else {
+          res.json(user)
+        }
+      })
+      .catch(next)
+  }
