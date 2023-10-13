86409b19191d4bc1a0c3f968b3904419629455a5
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-10-13 18:56:19 +0200

    personal-crud

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,80 +1,86 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const SALT_WORK_FACTOR = 10;
 
-const userSchema = new mongoose.Schema({
-
+const userSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: [true, 'Name is required'],
-        trim: true
+      type: String,
+      required: [true, "Name is required"],
+      trim: true,
     },
     surname: {
-        type: String,
-        required: [true, 'Surname is required'],
-        trim: true
+      type: String,
+      required: [true, "Surname is required"],
+      trim: true,
     },
     email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: true,
-        trim: true,
-        lowercase: true
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+      lowercase: true,
     },
     password: {
-        type: String,
-        required: [true, 'Password is required'],
+      type: String,
+      required: [true, "Password is required"],
     },
     phone: {
-        type: Number,
-        required: [true, 'Phone is required'],
-        trim: true
+      type: Number,
+      required: [true, "Phone is required"],
+      trim: true,
+    },
+    active: {
+      type: Boolean,
+      default: false,
     },
     role: {
-        type: String,
-        enum: ['ADMIN', 'SALES', 'PRODUCTION', 'WAREHOUSE', 'PURCHASING'],
-        default: 'USER'
+      type: String,
+      enum: ["ADMIN", "SALES", "PRODUCTION", "WAREHOUSE", "PURCHASING"],
+      default: "USER",
     },
     avatar: {
-        type: String,
-        default: 'https://res.cloudinary.com/dn6cd98sl/image/upload/v1614816220/avatars/default-avatar.png'
-    }
-}, {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dn6cd98sl/image/upload/v1614816220/avatars/default-avatar.png",
+    },
+  },
+  {
     timestamps: true,
     toJSON: {
-        virtuals: true,
-        transform: (doc, ret) => {
-            ret.id = doc.id;
-            delete ret._id;
-            delete ret.__v;
-            delete ret.password;
-            return ret;
-        }
-    }
-});
-
-userSchema.pre('save', function (next) {
-    const user = this;
-
-    if (user.isModified('password')) {
-        bcrypt.genSalt(SALT_WORK_FACTOR)
-            .then(salt => {
-                return bcrypt.hash(user.password, salt)
-                    .then(hash => {
-                        user.password = hash;
-                        next();
-                    });
-            })
-            .catch(error => next(error));
-    } else {
-        next();
-    }
-}
+      virtuals: true,
+      transform: (doc, ret) => {
+        ret.id = doc.id;
+        delete ret._id;
+        delete ret.__v;
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
 );
 
-userSchema.methods.checkPassword = function (passwordToCheck) {
-    return bcrypt.compare(passwordToCheck, this.password);
-}
+userSchema.pre("save", function (next) {
+  const user = this;
 
-const User = mongoose.model('User', userSchema);
+  if (user.isModified("password")) {
+    bcrypt
+      .genSalt(SALT_WORK_FACTOR)
+      .then((salt) => {
+        return bcrypt.hash(user.password, salt).then((hash) => {
+          user.password = hash;
+          next();
+        });
+      })
+      .catch((error) => next(error));
+  } else {
+    next();
+  }
+});
+
+userSchema.methods.checkPassword = function (passwordToCheck) {
+  return bcrypt.compare(passwordToCheck, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
 module.exports = User;
