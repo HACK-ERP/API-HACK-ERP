a97a09918f64593e6651430b6ab1dc0655e15740
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-10-15 19:41:47 +0200

    personal

--- Controllers/users.controller.js
+++ Controllers/users.controller.js
@@ -24,12 +24,12 @@ module.exports.create = (req, res, next) => {
 };
 
 module.exports.activate = (req, res, next) => {
-    User.findByIdAndUpdate(req.params.id, { active: true })
-      .then(() => {
-        res.redirect("http://localhost:5173/login");
-      })
-      .catch(next);
-  };
+  User.findByIdAndUpdate(req.params.id, { active: true })
+    .then(() => {
+      res.redirect("http://localhost:5173/login");
+    })
+    .catch(next);
+};
 
 module.exports.list = (req, res, next) => {
   User.find()
@@ -44,16 +44,50 @@ module.exports.list = (req, res, next) => {
 };
 
 module.exports.getOne = (req, res, next) => {
-    User.findById(req.params.id)
-        .then((user) => {
-            if(!user) {
-                next(createHttpError(StatusCodes.NOT_FOUND, 'User not found'))
-            } else {
-                res.status(StatusCodes.OK).json(user)
-            }
-        })
-        .catch(next);
-}
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        next(createHttpError(StatusCodes.NOT_FOUND, "User not found"));
+      } else {
+        res.status(StatusCodes.OK).json(user);
+      }
+    })
+    .catch(next);
+};
+
+module.exports.update = (req, res, next) => {
+  console.log("Update request received. ID:", req.params.id);
+  console.log("Update request body:", req.body);
+  try {
+    User.findByIdAndUpdate(req.params.id, req.body, {
+      runValidators: true,
+      new: true,
+    })
+      .then((user) => {
+        if (!user) {
+          throw createHttpError(StatusCodes.NOT_FOUND, "User not found");
+        } else {
+          console.log("User updated successfully:", user);
+          res.status(StatusCodes.OK).json(user);
+        }
+      })
+      .catch(next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.delete = (req, res, next) => {
+  User.findByIdAndDelete(req.params.id)
+    .then((user) => {
+      if (!user) {
+        next(createHttpError(StatusCodes.NOT_FOUND, "User not found"));
+      } else {
+        res.status(StatusCodes.OK).json(user);
+      }
+    })
+    .catch(next);
+};
 
 module.exports.getCurrentUser = (req, res, next) => {
   User.findById(req.currentUser)
