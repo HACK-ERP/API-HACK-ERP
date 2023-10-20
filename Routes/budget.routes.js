9e76693f2259ac82ce1cb05c4866d8afa96c6507
Juan Cruz <j.cruzb@outlook.com>
2023-10-20 10:56:55 +0200

    budgetDetail

--- Routes/budget.routes.js
+++ Routes/budget.routes.js
@@ -8,6 +8,7 @@ const budgetController = require('../Controllers/budget.controller');
 router.post('/budget/create', budgetController.create);
 router.get('/budget/list', budgetController.list);
 router.get('/budget/:id', budgetController.detail);
+console.log('budget.routes.js');
 router.patch('/budget/status/:id', budgetController.statusUpdate);
 router.put('/budget/:id', budgetController.update);
 router.delete('/budget/:id', budgetController.delete);
