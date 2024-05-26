656152b8d6785e3d4e53f59047abd526c835d4a0
Juan Carlos Cruz <j.cruzb@outlook.com>
2024-05-26 19:33:51 +0200

    Developer mode

--- Config/db.config.js
+++ Config/db.config.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const DB_NAME = 'HACK-ERP';
-const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
+// const URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017';
+const URI =  'mongodb://127.0.0.1:27017';
 const DB_URI = `${URI}/${DB_NAME}`;
 
 // Conexion a la BBDD
